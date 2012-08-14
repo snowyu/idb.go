629744fa3dd5579e4cb3ce8f93761df7661eb140
Riceball LEE <snowyu.lee@gmail.com>
2012-08-14 14:19:28 +0800

    * init

---
+++ constant.go
@@ -0,0 +1,9 @@
+/* the Constants of the iDB */
+package iDB
+
+const (
+    DBDirPerm       = 0755      //permission for creating database directory
+    DBFilePerm      = 0666      //permission for database file
+    XattrPrefix     = "user."   //the Xattr key name prefix
+)
+
---
+++ utils.go
@@ -0,0 +1,37 @@
+package iDB
+
+import (
+    //"os"
+    "xattr"
+)
+
+  func GetXattrKeys(aFile string) ([]string, error) {
+      keys, err := xattr.Listxattr(aFile)
+      var result []string
+      if err == nil {
+          for _, key := range keys {
+              if len(key)> len(XattrPrefix) && key[0:len(XattrPrefix)] == XattrPrefix {
+                  result = append(result, key[len(XattrPrefix):])
+              }
+          }
+      }
+      return result, err
+  }
+
+  func GetXattr(aFile, aKey string) (string, error) {
+      value, err := xattr.Getxattr(aFile, XattrPrefix+aKey)
+      return string(value), err
+  }
+
+  func IsXattrExists(aFile, aKey string) bool {
+      return xattr.IsXattrExists(aFile, XattrPrefix+aKey)
+  }
+
+  func SetXattr(aFile, aKey, aValue string) error {
+      return xattr.Setxattr(aFile, XattrPrefix+aKey, []byte(aValue))
+  }
+
+  func DeleteXattr(aFile, aKey string) error {
+      return xattr.Removexattr(aFile, XattrPrefix+aKey)
+  }
+
